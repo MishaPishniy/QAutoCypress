f33cb4001f0ef7f4df69a1f6cc7d6397403b11cd
MishaP <MishaP635@gmail.com>
2025-10-16 20:43:16 +0300

    Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,26 @@
 const { defineConfig } = require("cypress");
+const dotenv = require('dotenv')
+
+const envFile = `.env.${process.env.NODE_ENV || 'development'}`
+dotenv.config({ path: envFile })
 
 module.exports = defineConfig({
+  projectId: 'jezrdi',
   e2e: {
-    baseUrl:"https://example.cypress.io",
+    baseUrl: process.env.BASE_URL,
+    env: {
+      USER_NAME: process.env.USER_NAME,
+      USER_PASS: process.env.USER_PASS
+    },
     specPattern: "cypress/e2e/**/*.cy.js",
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
     viewportHeight:768 ,
     viewportWidth:1366,
     video: true,
@@ -13,6 +30,7 @@ module.exports = defineConfig({
     retries:{runMode:1 , openMode:0},
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
